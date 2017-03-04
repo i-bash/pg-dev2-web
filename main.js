e99573f78596038a4e20f3d0ec1c22bdf9d7ca1c
Ilya Bashtanov <bashtanov@gmail.com>
2017-03-04 18:49:59 +0300

    5 prevent parallel ajax requests

--- main.js
+++ main.js
@@ -1,86 +1,87 @@
-var lib={}
+var lib={
 
-$(()=>{
-	
-	lib.clearSql=()=>{
-		$('#sql').empty().hide();
-	};
-	
-	lib.server=(action,params,callback)=>{
-		$('#error,#success').hide();
-		$('#loader').show();
-		$.ajax({
-			method:'post',
-			url:'server.php?action='+action,
-			data:params,
-			success:(res)=>{
-				let separator='<br/>---<br/>';
-				if(res.sql.length){
-					let html=$('#sql').html();
-					$('#sql')
-						.fadeOut(
-							'slow',
-							()=>{
-								$('#sql')
-								.html(
-									res.sql.map(
-										s=>{
-											return s.trimLeft().replace(/\n/g,'<br/>').replace(/\t/g,'&nbsp;');
-										}
+	/**flag
+	 */
+	requestIsRunning:false,
+
+	/**server action
+	 * @param action - name of server action
+	 * @param params - parameters object
+	 * @param callback - function(data)
+	 * @return promise
+	 */
+	server: (action,params,callback)=>{
+		if(!lib.requestIsRunning){
+			$('#error,#success').hide();
+			$('#loader').show();
+			lib.requestIsRunning=true;
+			return $.ajax({
+				method:'post',
+				url:'server.php?action='+action,
+				data:params,
+				success:res=>{
+					let separator='<br/>---<br/>';
+					if(res.sql.length){
+						let html=$('#sql').html();
+						$('#sql')
+							.fadeOut(
+								'slow',
+								()=>{
+									$('#sql')
+									.html(
+										res.sql.map(
+											s=>{
+												return s.trimLeft().replace(/\n/g,'<br/>').replace(/\t/g,'&nbsp;');
+											}
+										)
+										.reverse()
+										.join(separator)
+										+(html&&res.sql?separator:'')
+										+html
 									)
-									.reverse()
-									.join(separator)
-									+(html&&res.sql?separator:'')
-									+html
-								)
-								.fadeIn('slow')
-							}
-						)
-					;
-				}
-				if(res.err===null){
-					if(callback!==undefined){
-						callback(res.data);
+									.fadeIn('slow')
+								}
+							)
+						;
 					}
-					//$('#success').slideDown('slow');
+					lib.requestIsRunning=false;
+					if(res.err===null){
+						if(callback!==undefined){
+							callback(res.data);
+						}
+					}
+					else{
+						$('#error').html(
+							res.err.message.replace(/\n\s*\^/,'').replace(/\n/g,'<br/>')
+						).slideDown('slow');
+					}
+				},
+				error:e=>{
+					lib.requestIsRunning=false;
+					alert('Unexpected server error');
+					console.error(e);
+				},
+				complete:()=>{
+					$('#loader').fadeOut('slow');
 				}
-				else{
-					$('#error').html(
-						res.err.message.replace(/\n\s*\^/,'').replace(/\n/g,'<br/>')
-					).slideDown('slow');
-				}
-				$('#loader').fadeOut('slow');
-			},
-			error:(e)=>{
-				alert('Unexpected server error');
-				console.error(e);
-			}
-		});
-	}
+			});
+		}
+	},
 
-	lib.displayPage=page=>{
+	//display app page
+	displayPage: page=>{
 		$.ajax('pages/'+page+'.html')
 			.then(
 				html=>{
 					$('#page').hide().html(html).fadeIn().removeClass().addClass(page);
-					//return $.ajax('pages/'+page+'.js');
 				},
 				()=>{$('#page').html('Page "'+page+'" not found');}
 			)
-/*
-			.then(
-				js=>{
-					$('#page-js').html(js);
-				},
-				()=>{console.warn(page+'.js not found');}
-			)
-*/
 		;
-		
-	}
-	
+	},
+
 	//turn form into ajax
-	lib.ajaxForm=(selector,callback,extraPars=[])=>{
+	ajaxForm: (selector,callback,extraPars=[])=>{
 		$(document).off('submit',selector);
 		$(document).on(
 			'submit',
@@ -97,11 +98,16 @@ $(()=>{
 				);
 			}
 		);
-	}
+	},
 
 	//display alert
-	lib.alert=(message,style='info')=>{
+	alert: (message,style='info')=>{
 		$('<div/>',{class:'alert alert-'+style+' fade'}).html(message).appendTo('#alert').addClass('in').delay(2000).slideUp('slow',function(){$(this).remove();});
-	}
-});
+	},
 
+	//clear contents of sql panel
+	clearSql: ()=>{
+		$('#sql').empty().hide();
+	}
+
+};
