67f90e613b77794c5c37447b59870068cefb8c67
Ilya Bashtanov <bashtanov@gmail.com>
2020-01-27 23:45:29 +0300

    correct postgres objects

--- sql/postgres.sql
+++ sql/postgres.sql
@@ -5,3 +5,11 @@ as $$
 	select set_config('log_min_duration_statement',case when enable then '0' else '-1' end,true);
 $$;
 grant execute on function trace to web,emp;
+
+alter role emp login;
+alter role web login;
+
+grant usage on schema empapi to emp;
+grant usage on schema webapi to web;
+
+alter function empapi.get_catalog security definer;
